ef88bd2ad672a5163a2e91f604965f72d18b081c
Sadar Khan <sadarkhan@abtach.org>
2018-12-20 12:29:45 +0500

    polymorphism changes

--- polymorphism.php
+++ polymorphism.php
@@ -6,23 +6,59 @@ ini_set('display_errors', 1);
 <html>
 
 <head>
-    <title>Laet Static Binding</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<title>Polymorphism</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<style>
+		label {
+			margin-top: 5px;
+		}
+	</style>
 </head>
 <!--
-	According to the Polymorphism principle, methods in different classes that do similar things should have the same name.
+According to the Polymorphism principle, methods in different classes that do similar things should have the same name.
 -->
 <body>
-<div class="container">
-	<h1>Polymorphism</h1>
-	<div class="col-md-12">
+	<div class="container">
+		<form action = "<?=$_SERVER['PHP_SELF']?>" method="POST" accept-charset="utf-8">
+				<div class="row">
+			<div class="col-md-6">
+				<h5>Full Time</h5><hr />
+				<div class="form-group">
+					<label>Name</label>
+					<input type="text" class="form-control" name="name" placeholder="Name">		
+					<label>Salary</label>
+					<input type="text" class="form-control" name="salary" placeholder="Salary">		
+					<label>Extra Payment</label>
+					<input type="text" class="form-control" name="xtraPayment" placeholder="Name">
+				</div>
+			</div>			
+			<div class="col-md-6">
+				<h5>Hours Based Employee</h5><hr />
+				<div class="form-group">
+					<label>Name</label>
+					<input type="text" class="form-control" name="HBEname" placeholder="Name">		
+					<label>Hours</label>
+					<input type="text" class="form-control" name="hours" placeholder="Hours">			
+					<label>Hour Rate</label>
+					<input type="text" class="form-control" name="hourRate" placeholder="Hour Rate">		
+					<label>Extra Payment</label>
+					<input type="text" class="form-control" name="HBExtraPayment" placeholder="Extra Payment">
+				</div>
+				<div class="form-group text-right">
+					<button type="submit" class="btn btn-success">Submit</button>
+				</div>
+			</div>
+	</div>
+			</form>
+
+
 		<?php
 		interface Employee {
 			public function monthlySalary();
@@ -66,16 +102,22 @@ ini_set('display_errors', 1);
 				return $salary;
 			}	
 		}
-$objFullTimeEmployee = new fullTimeEmployee(50000,5000);
-$objhourBasedEmployee = new hourBasedEmployee(90,1000,10000);
+		if(isset($_POST['name'])){
+				$objFullTimeEmployee = new fullTimeEmployee($_POST['salary'],$_POST['xtraPayment']);	
+				$objhourBasedEmployee = new hourBasedEmployee($_POST['hours'],$_POST['hourRate'],$_POST['HBExtraPayment']);
+		?>
+		<div class="row">
+			<div class="col-md-6">
+		<label>Name: </label><?= $objFullTimeEmployee->getEmployeeDetails($_POST['name']); ?><br />
+		<label>Salary: </label><?= $objFullTimeEmployee->monthlySalary(); ?></div>
+		<div class="col-md-6">
+		<label>Name: </label><?= $objhourBasedEmployee->getEmployeeDetails($_POST['HBEname']); ?><br />
+		<label>Salary: </label><?= $objhourBasedEmployee->monthlySalary(); ?></div></div>
+	</div>	
+	<?php
+		}
 ?>
-<h4>Full Time Employee</h4><hr />
-<label>Name: </label><?= $objFullTimeEmployee->getEmployeeDetails("John Doe"); ?><br />
-<label>Salary: </label><?= $objFullTimeEmployee->monthlySalary(); ?>
-<h4>Hours Based Employee</h4><hr />
-<label>Name: </label><?= $objhourBasedEmployee->getEmployeeDetails("Tony Stark"); ?><br />
-<label>Salary: </label><?= $objhourBasedEmployee->monthlySalary(); ?>
-	</div>
-</div>
+	
+
 </body>
 </html>
